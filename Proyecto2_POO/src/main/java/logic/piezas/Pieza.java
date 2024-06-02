90a9b9260288254bdb8a1672908edb0fd8751990
ArguedasG <152649491+ArguedasG@users.noreply.github.com>
2024-06-02 14:52:55 -0600

    Se edita la clase Pieza

--- Proyecto2_POO/src/main/java/logic/piezas/Pieza.java
+++ Proyecto2_POO/src/main/java/logic/piezas/Pieza.java
@@ -3,43 +3,59 @@ package logic.piezas;
 import java.awt.Color;
 import java.util.ArrayList;
 import logic.Tablero;
-
+ 
 public abstract class Pieza {
     
     protected boolean fueTomada;
     protected boolean fueMovida;
     protected Color color;
     protected Tablero tablero;
+    protected int posX;
+    protected int posY;
     
-    
-    public Pieza (Color color) {
-        
+    public Pieza (Color color, Tablero tablero, int posX, int posY) {
+        this.color = color;
+        this.tablero = tablero;
+        this.posX = posX;
+        this.posY = posY;
+        this.fueMovida = false;
+        this.fueTomada = false;
     }
     
-    public ArrayList<Integer[]> movimientos() {
-        return new ArrayList();
-    }
+    public abstract ArrayList<Integer[]> movimientos();
     
-    public abstract void mover(int posX, int posY);
+    public void mover(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+        this.fueMovida = true;
+    }
     
     public Integer[] getPosicion() {
-        return new Integer[2];
+        return new Integer[]{posX, posY};
     }
     
     public Color getColor() {
-        return Color.WHITE;
+        return this.color;
     }
     
     public boolean getMovida() {
-        return false;
+        return this.fueMovida;
     }
     
     public boolean getTomada() {
-        return false;
+        return this.fueTomada;
     }
     
     public void setTomada(boolean tomada) {
-        
+        this.fueTomada = tomada;
+    }
+
+    public void setY(int posY) {
+        this.posY = posY;
+    }
+
+    public void setX(int posX) {
+        this.posX = posX;
     }
     
 }
