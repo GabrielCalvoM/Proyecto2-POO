22ca3c44a4c9fd6adf58ec620c625812c175af48
ArguedasG <gaarguedas@estudiantec.cr>
2024-06-04 14:48:13 -0600

    Clases Alfil, Dama y Caballo modificadas

--- Proyecto2_POO/src/main/java/logic/piezas/Caballo.java
+++ Proyecto2_POO/src/main/java/logic/piezas/Caballo.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package logic.piezas;
 
-/**
- *
- * @author 50662
- */
-public class Caballo {
+import java.awt.Color;
+import java.util.ArrayList;
+import logic.Tablero;
+
+public class Caballo extends Pieza {
+
+    public Caballo(Color color, Tablero tablero, int posX, int posY) {
+        super(color, tablero, posX, posY);
+    }
+
+    @Override
+    public ArrayList<Integer[]> movimientos() {
+        ArrayList<Integer[]> movimientos = new ArrayList<>();
+
+        int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
+        int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
+
+        for (int i = 0; i < 8; i++) {
+            int newX = posX + dx[i];
+            int newY = posY + dy[i];
+
+            if (tablero.esValida(newX, newY) && (!tablero.estaOcupada(newX, newY) || tablero.estaOcupadaPorColorContrario(newX, newY, this.color))) {
+                movimientos.add(new Integer[]{newX, newY});
+            }
+        }
+
+        return movimientos;
+    }
     
 }
--- Proyecto2_POO/src/main/java/logic/piezas/Dama.java
+++ Proyecto2_POO/src/main/java/logic/piezas/Dama.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package logic.piezas;
 
-/**
- *
- * @author 50662
- */
-public class Dama {
+import java.awt.Color;
+import java.util.ArrayList;
+import logic.Tablero;
+
+public class Dama extends Pieza {
+
+    public Dama(Color color, Tablero tablero, int posX, int posY) {
+        super(color, tablero, posX, posY);
+    }
+
+    @Override
+    public ArrayList<Integer[]> movimientos() {
+        ArrayList<Integer[]> movimientos = new ArrayList<>();
+
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int i = 0; i < 8; i++) {
+            int newX = posX;
+            int newY = posY;
+
+            while (true) {
+                newX += dx[i];
+                newY += dy[i];
+
+                if (!tablero.esValida(newX, newY) || tablero.estaOcupada(newX, newY)) {
+                    if (tablero.estaOcupadaPorColorContrario(newX, newY, this.color)) {
+                        movimientos.add(new Integer[]{newX, newY});
+                    }
+                    break;
+                }
+
+                movimientos.add(new Integer[]{newX, newY});
+            }
+        }
+
+        return movimientos;
+    }
     
 }
